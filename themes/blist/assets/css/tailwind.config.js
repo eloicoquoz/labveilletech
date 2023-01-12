431ab4eaea98d123a77a491e5cff9b2e12bf38f0
Eloi Coquoz <eloi.coquoz@bluewin.ch>
2023-01-12 13:11:05 +0100

    Change Color

--- themes/blist/assets/css/tailwind.config.js
+++ themes/blist/assets/css/tailwind.config.js
@@ -20,83 +20,79 @@ module.exports = {
     extend: {
       typography: (theme) => ({
         DEFAULT: {
-          css: [
-            {
-              'code::before': {
-                content: '""',
-              },
-              'code::after': {
-                content: '""',
-              },
-            }
-          ]
+          css: [{
+            'code::before': {
+              content: '""',
+            },
+            'code::after': {
+              content: '""',
+            },
+          }]
         },
         dark: {
-          css: [
-            {
-              color: theme("colors.gray.400"),
-              '[class~="lead"]': {
-                color: theme("colors.gray.300"),
-              },
-              a: {
-                color: theme("colors.white"),
-              },
-              strong: {
-                color: theme("colors.white"),
-              },
-              "ol > li::before": {
-                color: theme("colors.gray.400"),
-              },
-              "ul > li::before": {
-                backgroundColor: theme("colors.gray.600"),
-              },
-              hr: {
-                borderColor: theme("colors.gray.200"),
-              },
-              blockquote: {
-                color: theme("colors.gray.200"),
-                borderLeftColor: theme("colors.gray.600"),
-              },
-              h1: {
-                color: theme("colors.white"),
-              },
-              h2: {
-                color: theme("colors.white"),
-              },
-              h3: {
-                color: theme("colors.white"),
-              },
-              h4: {
-                color: theme("colors.white"),
-              },
-              "figure figcaption": {
-                color: theme("colors.gray.400"),
-              },
-              code: {
-                color: theme("colors.white"),
-              },
-              "a code": {
-                color: theme("colors.white"),
-              },
-              'code::before': {
-                content: '""',
-              },
-              'code::after': {
-                content: '""',
-              },
-              pre: {
-                color: theme("colors.gray.200"),
-                backgroundColor: theme("colors.gray.800"),
-              },
-              thead: {
-                color: theme("colors.white"),
-                borderBottomColor: theme("colors.gray.400"),
-              },
-              "tbody tr": {
-                borderBottomColor: theme("colors.gray.600"),
-              },
+          css: [{
+            color: theme("colors.gray.400"),
+            '[class~="lead"]': {
+              color: theme("colors.gray.300"),
             },
-          ],
+            a: {
+              color: theme("colors.white"),
+            },
+            strong: {
+              color: theme("colors.white"),
+            },
+            "ol > li::before": {
+              color: theme("colors.gray.400"),
+            },
+            "ul > li::before": {
+              backgroundColor: theme("colors.gray.600"),
+            },
+            hr: {
+              borderColor: theme("colors.gray.200"),
+            },
+            blockquote: {
+              color: theme("colors.gray.200"),
+              borderLeftColor: theme("colors.gray.600"),
+            },
+            h1: {
+              color: theme("colors.white"),
+            },
+            h2: {
+              color: theme("colors.white"),
+            },
+            h3: {
+              color: theme("colors.white"),
+            },
+            h4: {
+              color: theme("colors.white"),
+            },
+            "figure figcaption": {
+              color: theme("colors.gray.400"),
+            },
+            code: {
+              color: theme("colors.white"),
+            },
+            "a code": {
+              color: theme("colors.white"),
+            },
+            'code::before': {
+              content: '""',
+            },
+            'code::after': {
+              content: '""',
+            },
+            pre: {
+              color: theme("colors.gray.200"),
+              backgroundColor: theme("colors.gray.800"),
+            },
+            thead: {
+              color: theme("colors.white"),
+              borderBottomColor: theme("colors.gray.400"),
+            },
+            "tbody tr": {
+              borderBottomColor: theme("colors.gray.600"),
+            },
+          }, ],
         },
       }),
     },
